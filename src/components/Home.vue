ef1b941ef1861e5277d22f8e05366fba88b8df44
Leta Keane <leta@hey.com>
2024-02-10 01:20:42 -0700

    Route homepage button to booking form

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -12,7 +12,7 @@
       <v-container class="text-center banner-container">
         <h1 class="text-lg-h1 text-md-h2 text-sm-h3 font-weight-black text-pink-lighten-1 mt-16">Claudia Mars Ink</h1>
         <p class="font-weight-bold text-pink-lighten-2 mb-4">tattoo artist 💜 boulder co</p>
-        <v-btn class="mb-16 rounded-lg" color="teal-lighten-1" prepend-icon="mdi-calendar-star-outline">Book a tattoo appointment!</v-btn>
+        <v-btn class="mb-16 rounded-lg" color="teal-lighten-1" prepend-icon="mdi-calendar-star-outline" to="/book">Book a tattoo appointment!</v-btn>
       </v-container>
     </v-card>
   </v-main>
